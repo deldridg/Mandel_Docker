a1963bd321cbc7d3533cd18bf96fce558fa4b502
David Eldridge <deldridg@ozemail.com.au>
2025-03-11 18:11:05 +1100

    ALLOWED_HOSTS update

--- mandel_docker/mandel_docker/settings.py
+++ mandel_docker/mandel_docker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x8z!6h+b5$7%$u($h2s_txql2j0_j%&0tzl)&8(qdomjc678wl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.235.232.125', 'localhost', '127.0.0.1']
 
 
 # Application definition
